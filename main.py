2271df4d7834317b3640fb7f9efca8cb6491945c
Derian Fernando <derian.sobral@gmail.com>
2018-09-28 16:00:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFilter,ImageDraw
+# Script pega uma imagem de entrada, guarda a imagem e a partir dela cria-se outra imagem identica de menor tamanho e com marca d'agua
 im = Image.open("porshe.jpg")
 print ('Dimensões: ',im.size, im.mode)
 im=im.resize((1200,593)) # Redimensionar a foto em um tamanho menor
